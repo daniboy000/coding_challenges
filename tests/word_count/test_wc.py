d677a95bb5473c4c6f5c2884e9a516955b79a023
Daniel Besen de Aguiar <daniel.bdaguiar@gmail.com>
2024-01-11 14:58:09 -0300

    Remove commented code

--- tests/word_count/test_wc.py
+++ tests/word_count/test_wc.py
@@ -5,34 +5,7 @@ def test_calc_word_count():
     """
     Test the calc_word_count function.
     """
-    assert calc_word_count("Hello World") == 3
+    assert calc_word_count("Hello World") == 2
     assert calc_word_count("Hello World!") == 2
     assert calc_word_count("Hello World!!") == 2
     assert calc_word_count("Hello World!!!") == 2
-    # assert calc_word_count("Hello World!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!") == 2
-    # assert calc_word_count("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!") == 2
