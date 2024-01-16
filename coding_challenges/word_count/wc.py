4b8f7849595ed882667e5ca1d472263613d440f3
Daniel Besen de Aguiar <daniel.bdaguiar@gmail.com>
2024-01-16 16:29:45 -0300

    Add get file size function with test

--- coding_challenges/word_count/wc.py
+++ coding_challenges/word_count/wc.py
@@ -1,5 +1,15 @@
-def calc_word_count(text):
+import os
+
+
+def calc_word_count(text: str) -> int:
     """
     Calculate the number of words in a text.
     """
     return len(text.split())
+
+
+def get_file_size(file_path: str) -> int:
+    """
+    Get the size of a file in bytes.
+    """
+    return os.path.getsize(file_path)
--- tests/word_count/test_wc.py
+++ tests/word_count/test_wc.py
@@ -1,4 +1,4 @@
-from coding_challenges.word_count.wc import calc_word_count
+from coding_challenges.word_count.wc import calc_word_count, get_file_size
 
 
 def test_calc_word_count():
@@ -6,6 +6,10 @@ def test_calc_word_count():
     Test the calc_word_count function.
     """
     assert calc_word_count("Hello World") == 2
-    assert calc_word_count("Hello World!") == 2
-    assert calc_word_count("Hello World!!") == 2
-    assert calc_word_count("Hello World!!!") == 2
+
+
+def test_get_file_size():
+    """
+    Test the get_file_size function.
+    """
+    assert get_file_size("tests/test.txt") == 342190
